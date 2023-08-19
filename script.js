258f8f5408d12371af588a236e45dcc53e2cf781
devjunior30 <edyjunior299@gmail.com>
2023-08-19 02:05:45 -0300

    iniciando o javaScript e alterando a img, pois estava com bug

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let primeiroNumero = document.querySelector("#firstNumber".value)
 let segundoNumero = document.querySelector("#secondNumber".value)
 let botao = document.querySelector("button")
+
+function sortear() {
+    Math.random(primeiroNumero)
+}
