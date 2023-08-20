100ec1b4c70802e1e3b92b60350d0bd653b20ad0
devjunior30 <edyjunior299@gmail.com>
2023-08-20 00:05:36 -0300

    finalização do projeto com novas regras no js e alteração no css

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ if(primeiroNumero >= segundoNumero) {
 
 resultado.innerHTML= "O PRIMEIRO NUMERO NÃO PODE SER MAIOR QUE O SEGUNDO NUMERO"
 
-} else { let result = Math.floor(Math.random() * (segundoNumero - primeiroNumero + 1)) + primeiroNumero;
+} else if (primeiroNumero && segundoNumero < 10) {
+
+    resultado.innerHTML= "O numero precisa ser maior que 10 (dez)"
+
+}
+
+else { let result = Math.floor(Math.random() * (segundoNumero - primeiroNumero + 1)) + primeiroNumero;
 
     resultado.innerHTML = `O numero sorteado foi: ` + result}
 
